bb921f2d20e03bd5b0dc506b41a2f7f99ef46b38
Alf Olsen <alf.olsen@hafslundeco.no>
2022-12-01 11:23:32 +0100

    done day 1

---
+++ day1.py
@@ -0,0 +1,19 @@
+
+with open('inputday1.txt', 'r') as file:
+    data = file.read().split('\n')
+    
+i = 0
+elf = [0]
+
+for line in data:
+    if len(line)>0:
+        elf[int(i)] = elf[int(i)] + int(line)
+    elif len(line)==0:
+        i = i + 1
+        elf.append(int(0)) 
+
+elf.sort(reverse=True)
+print("Svar del 1", elf[0])
+
+topp3 = elf[0] + elf[1] + elf[2]
+print("svar del 2", topp3)
